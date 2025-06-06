138fbb9ab73527cc1e68194174d850c6864d3ebe
aril114 <125197457+aril114@users.noreply.github.com>
2025-06-06 14:13:46 +0900

    Добавлено отображение числа комментариев

--- Models/Article.cs
+++ Models/Article.cs
@@ -8,5 +8,6 @@
 /// <param name="rating">Рейтинг</param>
 /// <param name="content">Текст в формате HTML</param>
 /// <param name="title">Заголовок</param>
+/// <param name="comment_count">Количество комментариев</param>
 /// <param name="login_Users">Логин автора</param>
 public record Article(int id, DateTime published_at, int rating, string content, string title, string excerpt, string preview_img, int comment_count, string login_Users);
